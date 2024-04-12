9429e685088b866af6816699f09ae3146accbd55
Ivan Aksenov <ivan120536@gmail.com>
2024-04-13 00:11:22 +0300

    feat(scheduling): Включить scheduling на уровне аннотаций

--- src/main/java/com/itc/StockHouse/StockHouseApplication.java
+++ src/main/java/com/itc/StockHouse/StockHouseApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class StockHouseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StockHouseApplication.class, args);
