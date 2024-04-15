2703af3ecacf56cdf9e00fd6d875c771cde7a751
Ivan Aksenov <ivan120536@gmail.com>
2024-04-15 23:13:19 +0400

    feat(metrics): Добавить аспект LogMethodExecutionAspect с логикой замерения выполнения метода

---
+++ src/main/java/com/itc/StockHouse/support/LogMethodExecutionAspect.java
@@ -0,0 +1,22 @@
+package com.itc.StockHouse.support;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogMethodExecutionAspect {
+    @Around("@annotation(LogMethodExecutionTime)")
+    public Object logMethodExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.nanoTime();
+
+        Object proceed = joinPoint.proceed();
+
+        long executionTime = System.nanoTime() - start;
+
+        System.out.printf("Метод %s выполнился за %d ms %n", joinPoint.getSignature(), executionTime / 1_000_000);
+        return proceed;
+    }
+}
