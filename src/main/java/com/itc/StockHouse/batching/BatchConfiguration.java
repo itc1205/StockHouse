6db3a0d69739d76347805afa75f797509c3eeefe
Ivan Aksenov <ivan120536@gmail.com>
2024-04-20 19:55:18 +0400

    refactor: Убрать ненужные импорты

--- src/main/java/com/itc/StockHouse/batching/BatchConfiguration.java
+++ src/main/java/com/itc/StockHouse/batching/BatchConfiguration.java
@@ -5,6 +5,7 @@ import com.itc.StockHouse.processors.StockItemPriceProcessor;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
