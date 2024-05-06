010415f36ed2f8450bc6d59e20fa96b84a8e8334
Ivan <ivan120536@gmail.com>
2024-05-06 22:49:14 +0400

    fix: Use enum name and shortened operator name instead of array with allowed strings

--- src/main/java/com/itc/StockHouse/dto/search/OperationDTO.java
+++ src/main/java/com/itc/StockHouse/dto/search/OperationDTO.java
@@ -6,21 +6,20 @@ import com.fasterxml.jackson.core.JsonParseException;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
-import java.util.List;
 
 @RequiredArgsConstructor
 public enum OperationDTO {
-    EQUAL(Arrays.asList("=", "EQUAL")),
-    LIKE(Arrays.asList("~", "LIKE")),
-    GREATER_THAN_OR_EQ(Arrays.asList(">=", "GREATER_THAN_OR_EQ")),
-    LESS_THAN_OR_EQ(Arrays.asList("<=", "LESS_THAN_OR_EQ"));
+    EQUAL("="),
+    LIKE("~"),
+    GREATER_THAN_OR_EQ(">="),
+    LESS_THAN_OR_EQ("<=");
 
-    private final List<String> allowedStrings;
+    private final String shortenedOperatorName;
 
     @JsonCreator
     public static OperationDTO fromString(@JsonProperty String name) throws JsonParseException {
         for (OperationDTO op : OperationDTO.values()) {
-            if (op.allowedStrings.contains(name)) {
+            if (op.shortenedOperatorName.equals(name) | op.name().equals(name)) {
                 return op;
             }
         }
