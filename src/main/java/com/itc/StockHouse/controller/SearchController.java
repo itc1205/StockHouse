a7028b9d573ed3e66ab92f1d9a057a4eea763b7e
Ivan <ivan120536@gmail.com>
2024-05-06 23:13:43 +0400

    refactor: replace wildcard import with single class imports

--- src/main/java/com/itc/StockHouse/controller/SearchController.java
+++ src/main/java/com/itc/StockHouse/controller/SearchController.java
@@ -10,7 +10,12 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
