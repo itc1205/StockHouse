a257e5c1718b83a5c86498d859af42042a28be68
Ivan <ivan120536@gmail.com>
2024-05-06 18:16:37 +0400

    refactor: Remove unused `Autowired` import

--- src/main/java/com/itc/StockHouse/controller/StockController.java
+++ src/main/java/com/itc/StockHouse/controller/StockController.java
@@ -10,7 +10,6 @@ import com.itc.StockHouse.service.StockService;
 import com.itc.StockHouse.utils.StockMappingUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/itc/StockHouse/service/StockServiceImpl.java
+++ src/main/java/com/itc/StockHouse/service/StockServiceImpl.java
@@ -6,7 +6,6 @@ import com.itc.StockHouse.model.StockEntity;
 import com.itc.StockHouse.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
