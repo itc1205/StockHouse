3b8eedbb0bd0cdc28cbc9d695841cb6482a7bc44
Ivan <ivan120536@gmail.com>
2024-05-14 23:52:14 +0400

    refactor: some cleanup

--- src/main/java/com/itc/StockHouse/controller/CustomerController.java
+++ src/main/java/com/itc/StockHouse/controller/CustomerController.java
@@ -2,8 +2,6 @@ package com.itc.StockHouse.controller;
 
 import com.itc.StockHouse.dto.domain.customer.CustomerDTO;
 import com.itc.StockHouse.dto.schema.customer.CreateCustomerSchema;
-import com.itc.StockHouse.exceptions.customer.CustomerAlreadyExistsException;
-import com.itc.StockHouse.exceptions.customer.CustomerNotFoundException;
 import com.itc.StockHouse.service.customer.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
@@ -30,6 +28,4 @@ public class CustomerController {
                         .build()
         );
     }
-
-
 }
