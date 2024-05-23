31b63e3e03a501b1f0cc20a4301de92c234ee40d
Ivan <ivan120536@gmail.com>
2024-05-23 11:47:44 +0400

    fix: Add `@Component` to `RestProperty`

--- src/main/java/com/itc/StockHouse/configurations/property/RestProperty.java
+++ src/main/java/com/itc/StockHouse/configurations/property/RestProperty.java
@@ -2,9 +2,11 @@ package com.itc.StockHouse.configurations.property;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 @Data
 @ConfigurationProperties(prefix = "rest")
+@Component
 public class RestProperty {
     ClientProperty currencyService;
 }
