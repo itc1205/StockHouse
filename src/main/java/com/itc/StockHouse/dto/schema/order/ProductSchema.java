b4ae5db2b0af9e75059068aefb017f7bc475d89f
Ivan <ivan120536@gmail.com>
2024-05-30 22:58:06 +0400

    fix: Fix bug when ProductSchema could not deserialize from file

--- src/main/java/com/itc/StockHouse/dto/schema/order/ProductSchema.java
+++ src/main/java/com/itc/StockHouse/dto/schema/order/ProductSchema.java
@@ -1,11 +1,13 @@
 package com.itc.StockHouse.dto.schema.order;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
 public class ProductSchema {
     @NotNull
     UUID productId;
