83aa9eeeccd9f798d8238c94f44510c5488cdac6
Ivan <ivan120536@gmail.com>
2024-05-30 23:02:01 +0400

    feat: Add event consumer

---
+++ src/main/java/com/itc/StockHouse/kafka/Consumer.java
@@ -0,0 +1,39 @@
+package com.itc.StockHouse.kafka;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itc.StockHouse.kafka.dto.KafkaEvent;
+import com.itc.StockHouse.kafka.handlers.KafkaEventHandler;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "app.kafka.enabled", matchIfMissing = false)
+public class Consumer {
+
+    private final Map<String, KafkaEventHandler> handlers;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @KafkaListener(topics = "${app.kafka.topic-name}", containerFactory = "kafkaListenerContainerFactoryByteArray")
+    public void listenGroupTopic(byte[] message) throws IOException {
+        log.info("Received message %s".formatted(Arrays.toString(message)));
+        try {
+            final KafkaEvent event = objectMapper.readValue(message, KafkaEvent.class);
+            if (!handlers.containsKey(event.getEvent())) {
+                throw new RuntimeException("No event handler for %s".formatted(event.getEvent()));
+            }
+            KafkaEventHandler eventHandler = handlers.get(event.getEvent());
+            eventHandler.handle(event);
+        } catch (Exception ex) {
+            log.warn("Could not process event: %s".formatted(ex.getMessage()));
+        }
+    }
+}
