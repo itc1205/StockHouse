cc7c4d836f05b8dffd74a5a96fe0d81b96bd90ab
Ivan <ivan120536@gmail.com>
2024-06-07 00:01:35 +0400

    feat: Add client for interacting with s3

---
+++ src/main/java/com/itc/StockHouse/client/s3/S3ImageClient.java
@@ -0,0 +1,14 @@
+package com.itc.StockHouse.client.s3;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.List;
+import java.util.UUID;
+
+public interface S3ImageClient {
+    UUID uploadImage(MultipartFile file);
+
+    ByteArrayOutputStream getImagesAsZip(List<UUID> uuidList);
+}
---
+++ src/main/java/com/itc/StockHouse/client/s3/S3ImageClientImpl.java
@@ -0,0 +1,90 @@
+package com.itc.StockHouse.client.s3;
+
+import com.itc.StockHouse.configurations.property.S3Property;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class S3ImageClientImpl implements S3ImageClient {
+
+    private static final String fileNameMetadata = "x-amz-meta-fileName";
+
+    private final S3Property s3Property;
+    private final S3Client s3Client;
+
+
+
+    @Override
+    public UUID uploadImage(MultipartFile file) {
+        UUID imageId = UUID.randomUUID();
+
+        Map<String, String> metadata = new HashMap<>() {{
+           put("x-amz-meta-fileName", file.getName());
+        }};
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(s3Property.getBucketName())
+                .key(imageId.toString())
+                .metadata(metadata)
+                .build();
+
+        try {
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+        } catch (IOException | S3Exception e) {
+            log.error("Could not upload file: {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return imageId;
+    }
+
+    @Override
+    public ByteArrayOutputStream getImagesAsZip(List<UUID> uuidList) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream);
+        try {
+            for (UUID uuid : uuidList) {
+
+
+                GetObjectRequest request = GetObjectRequest.builder()
+                        .key(uuid.toString())
+                        .bucket(s3Property.getBucketName())
+                        .build();
+
+                ResponseBytes<GetObjectResponse> responseBytes = s3Client.getObjectAsBytes(request);
+
+                ZipEntry zipEntry = new ZipEntry(responseBytes.response().metadata().get(fileNameMetadata));
+                zipOutputStream.putNextEntry(zipEntry);
+
+                zipOutputStream.write(responseBytes.asByteArray(), 0, responseBytes.asByteArray().length);
+
+                // Close the ZipEntry.
+                zipOutputStream.closeEntry();
+
+            }
+            zipOutputStream.close();
+
+        } catch (IOException | S3Exception e) {
+            log.error("Could not archive/download files: {}", e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return outputStream;
+    }
+}
